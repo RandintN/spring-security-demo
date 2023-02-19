de26c46cf2b2220f0e528cd9c4ba2cfcafd203ea
Robson Cassiano <robson.cordeiro777@gmail.com>
2023-02-19 16:33:35 -0300

    Initial commit

---
+++ src/main/java/simple/software/learn/springsecuritydemo/AuthController.java
@@ -0,0 +1,18 @@
+package simple.software.learn.springsecuritydemo;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import simple.software.learn.springsecuritydemo.service.TokenService;
+
+@RestController
+@RequestMapping(path = "/api/auth")
+public record AuthController(TokenService tokenService) {
+
+    @PostMapping(path = "/token")
+    public String token(Authentication authentication) {
+        return tokenService.generateToken(authentication);
+    }
+
+}
---
+++ src/test/java/simple/software/learn/springsecuritydemo/SpringSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package simple.software.learn.springsecuritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
